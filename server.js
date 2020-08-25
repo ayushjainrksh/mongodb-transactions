41ae7a2e4a24e760d21a08d47b3246ef83096d33
Ayush Jain <ayushjainrksh@gmail.com>
2020-08-25 19:22:27 +0530

    Add basic routes

--- server.js
+++ server.js
@@ -4,8 +4,46 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
+require('dotenv').config();
+
+app.use(bodyParser.json());
+mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const employeeSchema = new mongoose.Schema({
+    name: String,
+    dob: String,
+    isManager: Boolean,
+    department: String
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
 app.get('/', function(req, res){
     res.send("Hi");
+})
+
+app.get('/employee', async function(req, res){
+    try{
+        const employees = await Employee.find();
+        if(employees)
+            res.send({"employees": employees})
+        else
+            res.send({"employees": null})
+    } catch(err) {
+        res.send({"err": err})
+    }
+});
+
+app.post('/employee/create', async function(req, res){
+    try {
+        const employee = await Employee.create(req.body);
+        if(employee) {
+            employee.save();
+            res.send(employee);
+        }
+    } catch(err) {
+        res.send({"err": err})
+    }
 })
 
 app.listen(PORT, function(err){
