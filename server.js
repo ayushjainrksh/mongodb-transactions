3574bc9774e926851a5e165a657dbf8471385879
Ayush Jain <ayushjainrksh@gmail.com>
2020-08-26 18:11:14 +0530

    Refactor some code and add descriptions

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ const express = require('express'),
 
 require('dotenv').config();
 
+//Import models
 const Employee = require('./models/Employee');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 
-let database;
-let collection;
 let workers;
 
+//Set up mongodb connection
+let database;
+let collection;
 mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db){
     database = db;
+
+    //Create the collection if not exists (required when using transactions)
     db.db.listCollections().toArray(function(err, collectionNames){
         let exists = false;
         collectionNames.forEach(coll => {
@@ -33,54 +37,78 @@ mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: tr
         } else {
             collection = db.createCollection("employees");
         }
-    })
+    });
+
+    //Start the server after connection to mongodb is established
     app.listen(PORT, function(err){
-        if(err)
+        if(err) 
             console.log(err);
         console.log(`Server starte at ${PORT}...`);
     }) 
 });
 
-app.get('/', function(req, res){
-    res.send("Hi");
+// ========================
+//         ROUTES
+// ========================
+
+//Route to test if server is up
+app.get('/ping', function(req, res){
+    res.send('pong');
 })
 
+/**
+  GET '/employee'
+  To get data of all employees in json format
+ */
 app.get('/employee', async function(req, res){
     try{
         const employees = await Employee.find();
         if(employees)
-            res.send({"employees": employees})
+            res.send({'employees': employees, 'message': `Fetched ${employees.length} employees successfully`});
         else
-            res.send({"employees": null})
+            res.send({'employees': null, 'message': 'No employee exists'})
     } catch(err) {
-        res.send({"err": err})
+        res.send({'error': err, 'message': 'Encountered an error. Please try again.'});
     }
 });
 
+/**
+  POST '/employee/createOne'
+  To create a single employee manually
+ */
 app.post('/employee/createOne', async function(req, res){
     try {
         const session = await mongoose.startSession();
         session.startTransaction();
 
-        await Employee.create([req.body], {session: session});
+        await Employee.collection.insertOne(req.body, {session: session});
         await session.commitTransaction();
         session.endSession();
-        res.sendStatus(200);
+        res.send({'employee': employee, 'message': `Employee created successfully`});
     } catch(err) {
-        console.log(err)
-        res.send({"err": err})
+        res.send({"err": err, "message": "An error occurred while adding employee data"})
     }
 });
 
+/**
+  POST '/employee/create'
+  Creating employees in batch by uploading csv file
+ */
 app.post('/employee/create', async function(req, res){
     try {
+        //Check if the current thread is main thread or worker
         if(isMainThread) {
-            console.log('Creating a new worker');
-            
+
+            //Create a new thread to start the operation
+            console.log('Creating a new worker...');
             if (!req.files)
-                return res.status(400).send('No files were uploaded.');
-            var employeeFile = req.files.file;
-            var employees = [];
+                return res.send({'message': 'Opeartion failed. No files were uploaded.'});
+
+            //Get uploaded file from form
+            const employeeFile = req.files.file;
+            let employees = [];
+
+            //Use fast-csv library to parse csv content to json array
             csv
             .parseString(employeeFile.data.toString(), {
                 headers: true,
@@ -90,7 +118,7 @@ app.post('/employee/create', async function(req, res){
                 employees.push(data);
             })
             .on("end", function(){
-                console.log(employees);
+                //Create a new thread and send employee data
                 workers = new Worker('./workers.js', {
                     workerData: {
                         reqBody: employees
@@ -100,14 +128,17 @@ app.post('/employee/create', async function(req, res){
         } else {
             console.log("I am a worker");
         }
-        res.sendStatus(200);
+        res.send({'message': 'Opeartion successful'});
     } catch(err) {
-        console.log(err)
-        res.send({"err": err})
+        res.send({'err': err, 'message': 'Encountered an error while performing operation!'});
     }
 });
 
+/**
+  POST '/employee/create/cancel'
+  Cancel a batch employee creation request (Stop '/employee/create')
+ */
 app.post('/employee/create/cancel', function(req, res){
     workers.postMessage('cancel');
-    res.send("Stopped successfully");
-})
+    res.send({'message': 'Operation cancelled successfully!'});
+});
--- workers.js
+++ workers.js
@@ -1,12 +1,17 @@
 const worker = require('worker_threads');
 const {port1, port2} = new worker.MessageChannel();
 const mongoose = require('mongoose');
+
+//Import Models
 const Employee = require('./models/Employee');
 
+//Set up database connection for the workder
 let database;
 let collection;
 mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db){
     database = db;
+
+    //Create the collection if not exists (required when using transactions)
     db.db.listCollections().toArray(function(err, collectionNames){
         let exists = false;
         collectionNames.forEach(coll => {
@@ -14,49 +19,56 @@ mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: tr
                 exists = true;
         });
         if(exists)
-        {
             collection = db.collection('employees');
-        } else {
+        else
             collection = db.createCollection("employees");
-        }
     });
 });
 
+//To store mongodb session information
 let session;
 
+//Create an employee using mongodb transactions
 async function createEmployee(requestData) {
     try {
         session = await mongoose.startSession();
         session.startTransaction();
+        
+        //Insert data to the collection
         await Employee.collection.insert(requestData, {session: session});
+
+        //Wait for 5 seconds before committing to the database (emulate a long running transaction)
         console.log("Waiting...");
         setTimeout(async()=>{
+            //Commit the transaction if there is no error
             await session.commitTransaction();
             session.endSession();
-            console.log("Successful")
+            console.log("Employee data added successfully!");
         }, 5000);
     } catch(err) {
-        console.log(err);
+        //Abort and rollback the transaction in case of an error
         await session?.abortTransaction();
         session?.endSession();
-        console.log("Failed")
+        console.log("Failed to add employee data!");
     }
 }
 
+//Get data from the main thread
 const requestData = worker.workerData.reqBody;
-
 console.log(requestData);
 
+//Call function to start the transaction
 createEmployee(requestData);
 console.log(`Thread id: ${worker.threadId}`);
 
+//Listen for message on parentPort
 worker.parentPort.on('message', async function(message) {
-    console.log(message);
+    //If cancel message arrives, then stop the transaction
     if(message=='cancel'){
-        console.log('Cancelling in worker thread.');
+        console.log('Cancelling transaction...');
         session.endSession();
-        console.log('End session');
-        console.log('Kill thread: ', worker.threadId);
+        console.log('======Session ended======');
+        console.log('Killing thread with id: ', worker.threadId);
         process.exit(0);
     }
 });
